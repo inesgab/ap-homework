4c32c176dce3c27905536b8db50bc4862b6d6c22
Inès Gabert <ines.gabert@etu.minesparis.psl.eu>
2022-11-11 22:26:50 +0100

    shortest path and distance

--- graph_shortest_path/graph-shortest-path.py
+++ graph_shortest_path/graph-shortest-path.py
@@ -39,7 +39,96 @@ def reachables(graph, s, reached):
 # print(reachables(parse_graph("graph.csv"), 'd', set()))
 
 # exo 4
+import math
+
+def shortest_distance(graph, v1, v2):
+
+    # initialisation
+    # on se définit une variable locale à la fonction
+    # qui matérialise le marquage
+
+    visited = {}
+    visited[v1] = 0
+    # ensuite on fait une boucle jusqu'à ce que la condition soit remplie
+    while True:
+
+        # les arêtes qui satisfont le critère 
+        edges = {}
+
+        # on énumère toutes les arêtes, et on ajoute dans
+        # edges celles qui satisfont le critère
+        for i in visited:
+            if i in graph:
+                for j in graph[i]:
+                    if j not in visited:
+                        edges[j] = visited[i] + graph[i][j]
 
 
+        # si on n'a aucune arête c'est que c'est raté
+        if not edges:
+            return None
 
+        # sinon on trouve la meilleure
+        shortest_length = math.inf
+        shortest_vertex = None
+        for j in edges:
+            if edges[j] < shortest_length:
+                shortest_length = edges[j] # trouver la plus courte
+                shortest_vertex = j # et mémoriser le sommet correspondant
+
+        visited[shortest_vertex] = shortest_length # marquer le sommet correspondant
+
+        # regarder si c'est le sommet 
+        if shortest_vertex == v2:
+            return shortest_length
+
+# print(shortest_distance(G, 'a', 'f'))
+
+def path(parents, vinit, vfinal):
+    v = vfinal
+    path = [v]
+    while v != vinit:
+        v = parents[v]
+        path.append(v)
+    return list(reversed(path))
+
+def shortest_path(graph, v1, v2):
+    visited = {}
+    visited[v1] = 0
+    parents = {}
+    while True:
+
+        # les arêtes qui satisfont le critère 
+        edges = {}
+
+        # on énumère toutes les arêtes, et on ajoute dans
+        # edges celles qui satisfont le critère
+        for i in visited:
+            if i in graph:
+                for j in graph[i]:
+                    if j not in visited:
+                        edges[j] = (i, visited[i] + graph[i][j])
+
+
+        # si on n'a aucune arête c'est que c'est raté
+        if not edges:
+            return None
+
+        # sinon on trouve la meilleure
+        shortest_length = math.inf
+        shortest_vertex = None
+        for j in edges:
+            if edges[j][1] < shortest_length:
+                shortest_length = edges[j][1] # trouver la plus courte
+                shortest_vertex = j # et mémoriser le sommet correspondant
+
+        visited[shortest_vertex] = shortest_length # marquer le sommet correspondant
+        parents[shortest_vertex] = edges[shortest_vertex][0]
+        # regarder si c'est le sommet 
+        if shortest_vertex == v2:
+            return shortest_length, path(parents, v1, v2)
+
+print(shortest_path(G, 'a', 'e'))
+
+#exo 6
 
